3fbaf9d71225eab642f0083fa43680f90f060cb7
Daniel Alves Farias <daniel_h.m@outlook.com>
2022-03-22 10:20:57 -0300

    QoS changes and comma bug fix
    Now you can't use comma twice in a same value, i'm using a monospaced font now, the window is no longer resizable and can´t be maximized or minimized

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+from cgitb import text
 from tkinter import *
+from tkinter import font
 
 
 class Calculator:
     def __init__(self, janela):
         
         janela.title('Calculator 5000')
+        #janela.geometry('135x255')
+        janela.resizable(False, False)
+        janela.attributes('-toolwindow', True)
 
+        self.default_font = font.Font(family='Consolas', weight='bold')
 
-        self.display = Text(janela, width=13, height=2, font='Arial')
+        self.display = Text(janela, width=13, height=2, font=self.default_font)
         self.display.tag_configure('justify-configuration', justify='right')
         self.display.insert('0.0', '00')
         self.display.tag_add('justify-configuration', '0.0')
@@ -15,57 +21,57 @@ class Calculator:
 
         ##Number buttons
 
-        self.button1 = Button(janela, text='1', font='Arial', command=self.b1)
+        self.button1 = Button(janela, text='1', font=self.default_font, command=self.b1)
         self.button1.grid(column=0, row=4, padx=5, pady=5)
 
-        self.button2 = Button(janela, text='2', font='Arial', command=self.b2)
+        self.button2 = Button(janela, text='2', font=self.default_font, command=self.b2)
         self.button2.grid(column=1, row=4, padx=5, pady=5)
 
-        self.button3 = Button(janela, text='3', font='Arial', command=self.b3)
+        self.button3 = Button(janela, text='3', font=self.default_font, command=self.b3)
         self.button3.grid(column=2, row=4, padx=5, pady=5)
 
-        self.button4 = Button(janela, text='4', font='Arial', command=self.b4)
+        self.button4 = Button(janela, text='4', font=self.default_font, command=self.b4)
         self.button4.grid(column=0, row=3, padx=5, pady=5)
 
-        self.button5 = Button(janela, text='5', font='Arial', command=self.b5)
+        self.button5 = Button(janela, text='5', font=self.default_font, command=self.b5)
         self.button5.grid(column=1, row=3, padx=5, pady=5)
 
-        self.button6 = Button(janela, text='6', font='Arial', command=self.b6)
+        self.button6 = Button(janela, text='6', font=self.default_font, command=self.b6)
         self.button6.grid(column=2, row=3, padx=5, pady=5)
 
-        self.button7 = Button(janela, text='7', font='Arial', command=self.b7)
+        self.button7 = Button(janela, text='7', font=self.default_font, command=self.b7)
         self.button7.grid(column=0, row=2, padx=5, pady=5)
 
-        self.button8 = Button(janela, text='8', font='Arial', command=self.b8)
+        self.button8 = Button(janela, text='8', font=self.default_font, command=self.b8)
         self.button8.grid(column=1, row=2, padx=5, pady=5)
 
-        self.button9 = Button(janela, text='9', font='Arial', command=self.b9)
+        self.button9 = Button(janela, text='9', font=self.default_font, command=self.b9)
         self.button9.grid(column=2, row=2, padx=5, pady=5)
 
-        self.button0 = Button(janela, text='0', font='Arial', command=self.b0)
+        self.button0 = Button(janela, text='0', font=self.default_font, command=self.b0)
         self.button0.grid(column=0, row=5, columnspan=2, padx=5, pady=5, sticky=EW)
 
         ##Operation buttons
 
-        self.buttoncomma = Button(janela, text=',', font='Arial', command=self.b_comma)
+        self.buttoncomma = Button(janela, text=',', font=self.default_font, command=self.b_comma)
         self.buttoncomma.grid(column=2, row=5, padx=5, pady=5)
 
-        self.buttonequal = Button(janela, text='=', font='Arial')
+        self.buttonequal = Button(janela, text='=', font=self.default_font, command=self.b_equal)
         self.buttonequal.grid(column=3, row=4, rowspan=2, padx=5, pady=5, sticky=NS)
 
-        self.buttonplus = Button(janela, text='+', font='Arial', command=self.b_plus)
+        self.buttonplus = Button(janela, text='+', font=self.default_font, command=self.b_plus)
         self.buttonplus.grid(column=3, row=2, rowspan=2, padx=5, pady=5, sticky=NS)
 
-        self.buttonminus = Button(janela, text='-', font='Arial', command=self.b_minus)
+        self.buttonminus = Button(janela, text='-', font=self.default_font, command=self.b_minus)
         self.buttonminus.grid(column=3, row=1, padx=5, pady=5)
 
-        self.buttonmultiplication = Button(janela, text='*', font='Arial', command=self.b_multiplication)
+        self.buttonmultiplication = Button(janela, text='*', font=self.default_font, command=self.b_multiplication)
         self.buttonmultiplication.grid(column=2, row=1, padx=5, pady=5)
 
-        self.buttondivision = Button(janela, text='/', font='Arial', command=self.b_division)
+        self.buttondivision = Button(janela, text='/', font=self.default_font, command=self.b_division)
         self.buttondivision.grid(column=1, row=1, padx=5, pady=5)
 
-        self.buttonclear = Button(janela, text='C', font='Arial', command=self.clear_display)
+        self.buttonclear = Button(janela, text='C', font=self.default_font, command=self.clear_display)
         self.buttonclear.grid(column=0, row=1, padx=5, pady=5)
 
     def b1(self):
@@ -140,13 +146,27 @@ class Calculator:
     
     def b_comma(self):
         if self.display.get("1.0", "end-1c") != '00' and self.display.get("1.0", "end-1c")[-1] != ',' and self.display.get("1.0", "end-1c")[-1].isnumeric():
-            self.display.insert(END, ',')
-            self.display.tag_add('justify-configuration', '0.0')
+            self.status_comma = True
+            for j in reversed(self.display.get("1.0", "end-1c")):
+                if j == ',':
+                    self.status_comma = False
+                    break
+
+                elif j in ['*', '/', '+', '-']:
+                    break
+
+            if self.status_comma:
+                self.display.insert(END, ',')
+                self.display.tag_add('justify-configuration', '0.0')
+                self.status_comma = True
 
     def empty_verification(self):
         print(f'{self.display.get("1.0", "end-1c")}')
         if self.display.get("1.0", "end-1c") == '00':
             self.display.delete(1.0, END)
+    
+    def b_equal(self):
+        print(self.display.get("1.0", "end-1c"))
 
     def clear_display(self):
         self.display.delete(1.0, END)
